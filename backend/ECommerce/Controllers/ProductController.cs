38f511b813d414a0d9ce926a476307291f4e20d5
sanju8400 <Sanjusharma8400@gmail.com>
2024-07-31 22:01:29 -0400

    Added Product Controller and Service

---
+++ backend/ECommerce/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using ECommerce.Models;
+using ECommerce.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get() 
+           {
+          var result =  await _productService.Get();
+            return result;
+        } 
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        public async Task<ActionResult<Product>> Get(string id)
+        {
+            var product = await _productService.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Create(Product product)
+        {
+            try
+            {
+                await _productService.Create(product);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+           
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id.ToString() }, product);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Product productIn)
+        {
+            var product = await _productService.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _productService.Update(id, productIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var product = await _productService.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _productService.Remove(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ backend/ECommerce/Services/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Models;
+using MongoDB.Driver;
+namespace ECommerce.Services
+{
+    public class ProductService
+    {
+        private readonly IMongoCollection<Product> _products;
+
+        public ProductService(IMongoSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _products = database.GetCollection<Product>("Products");
+        }
+
+        public async Task<List<Product>> Get()
+        { 
+            return await _products.Find(product => true).ToListAsync();
+        }
+
+        public async Task<Product> Get(string id) =>
+            await _products.Find<Product>(product => product.Id == id).FirstOrDefaultAsync();
+
+        public async Task<Product> Create(Product product)
+        {
+            product.CreatedAt = DateTime.UtcNow;
+            product.UpdatedAt = DateTime.UtcNow;
+            await _products.InsertOneAsync(product);
+            return product;
+        }
+
+        public async Task Update(string id, Product productIn)
+        {
+            productIn.UpdatedAt = DateTime.UtcNow;
+            await _products.ReplaceOneAsync(product => product.Id == id, productIn);
+        }
+
+        public async Task Remove(string id) =>
+            await _products.DeleteOneAsync(product => product.Id == id);
+    }
+
+}
